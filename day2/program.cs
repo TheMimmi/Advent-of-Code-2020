020e5250d0a732cc7cf366b5e247d551becf9e05
Mimmi Sandström <49366362+TheMimmi@users.noreply.github.com>
2020-12-14 23:17:32 +0100

    Create program.cs

---
+++ day2/program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tasks.Part1();
+            Tasks.Part2();
+
+        }
+    }
+}
